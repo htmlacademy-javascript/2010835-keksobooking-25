336f3f7376551a698799132fab903f97e414a744
Keks <keks@htmlacademy.ru>
2022-03-01 16:43:35 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,14 +1,23 @@
-const type = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const timeValues = ['12:00', '13:00', '14:00'];
-const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photos = [
+const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TIME_VALUES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
-const authorIds = [];
+const LAT_MIN_VALUE = 35.65;
+const LAT_MAX_VALUE = 35.7;
+const LNG_MIN_VALUE = 139.7;
+const LNG_MAX_VALUE = 139.8;
+const MIN_ROOMS_NUMBER = 1;
+const MAX_ROOMS_NUMBER = 5;
+const MIN_GUESTS_NUMBER = 1;
+const MAX_GUESTS_NUMBER = 10;
+const MIN_PRICE = 1000;
+const MAX_PRICE = 10000;
+const LOCATION_DECIMAL_PLACES = 5;
 const ADVERTISEMENTS_COUNT = 10;
-const AUTHOR_ID_MAX_VALUE = 10;
 
 const getRandomInt = (min, max) => {
   if(min < 0 || max < 0){
@@ -53,58 +62,37 @@ const getRandomArrayElements = (array) => {
   return result;
 };
 
-const createAuthor = () => {
-  if(authorIds.length >= AUTHOR_ID_MAX_VALUE){
-    throw 'Список доступных id авторов исчерпан';
-  }
-
-  let id = getRandomInt(1, AUTHOR_ID_MAX_VALUE);
-
-  while(authorIds.includes(id)){
-    id = getRandomInt(1, AUTHOR_ID_MAX_VALUE);
-  }
-
-  authorIds.push(id);
-
-  id = id < 10 ? `0${String(id)}` : String(id);
-
-  const avatar = `img/avatars/user${String(id)}.png`;
-
-  return {
-    avatar
-  };
-};
+const createAuthor = (index) => ({
+  avatar: `img/avatars/user${String(++index).padStart(2, '0')}.png`,
+});
 
 const createLocation = () => ({
-  lat: getRandomFloat(35.65, 35.7, 5),
-  lng: getRandomFloat(139.7, 139.8, 5),
-
-  toString: function() { return `${this.lat}, ${this.lng}`; }
+  lat: getRandomFloat(LAT_MIN_VALUE, LAT_MAX_VALUE, LOCATION_DECIMAL_PLACES),
+  lng: getRandomFloat(LNG_MIN_VALUE, LNG_MAX_VALUE, LOCATION_DECIMAL_PLACES)
 });
 
 const createOffer = (location = createLocation()) => ({
   title: 'Уютные апартаменты',
-  address: location.toString(),
-  price: getRandomInt(1000, 10000),
-  type: getRandomArrayElement(type),
-  rooms: getRandomInt(1, 5),
-  guests: getRandomInt(1, 20),
-  checkin: getRandomArrayElement(timeValues),
-  checkout: getRandomArrayElement(timeValues),
-  features: getRandomArrayElements(features),
+  address: `${location.lat}, ${location.lng}`,
+  price: getRandomInt(MIN_PRICE, MAX_PRICE),
+  type: getRandomArrayElement(TYPE),
+  rooms: getRandomInt(MIN_ROOMS_NUMBER, MAX_ROOMS_NUMBER),
+  guests: getRandomInt(MIN_GUESTS_NUMBER, MAX_GUESTS_NUMBER),
+  checkin: getRandomArrayElement(TIME_VALUES),
+  checkout: getRandomArrayElement(TIME_VALUES),
+  features: getRandomArrayElements(FEATURES),
   description: 'Просторные апартаменты с видом на город',
-  photos: getRandomArrayElements(photos),
+  photos: getRandomArrayElements(PHOTOS),
 });
 
-const createAdvertisement = () => {
+const createAdvertisement = (element, index) => {
   const location = createLocation();
 
   return {
-    author: createAuthor(),
+    author: createAuthor(index),
     offer: createOffer(location),
     location,
   };
 };
 
 Array.from({length: ADVERTISEMENTS_COUNT}, createAdvertisement);
-
