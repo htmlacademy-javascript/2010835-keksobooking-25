ab9c0749eafcae86e72895f4d1a2d02fb0dba9b9
BSI <bsi_acs@mail.ru>
2022-03-22 14:09:44 +0300

    Реализовано: отображение карты; отбражение меток; задание поля адрес посредством перемещения главной метки.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
-import {createAdvertisement, ADVERTISEMENTS_COUNT} from './data.js';
-import {createRandomAdvertisementsCards} from './cards-generator.js';
 import'./form-validator.js';
+import'./map-creator.js';
+import {generateRandomAdvertisementsData} from './data.js';
+import {createRandomAdvertisementCard} from './cards-generator.js';
+import { deactivateForm, activateForm } from './form-activity-switcher.js';
+import {addMainMarker, addMarker, createMap} from './map-creator.js';
 
-const randomAdvertisementsCards = createRandomAdvertisementsCards(Array.from({length: ADVERTISEMENTS_COUNT}, createAdvertisement));
+const addAdvertisementsMarkers = (_randomData, _map) => {
+  _randomData.forEach((element) => {
+    addMarker({lat: element.location.lat, lng: element.location.lng}, _map, createRandomAdvertisementCard(element));
+  });
+};
 
-const mapCanvas = document.querySelector('.map__canvas');
-mapCanvas.appendChild(randomAdvertisementsCards[0]);
+const markerMoveEndHandler = (evt) => {
+  const adressInput = document.querySelector('#address');
+  const address = `${evt.target.getLatLng().lat.toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
+  adressInput.value = address;
+};
+
+deactivateForm();
+
+const randomData = generateRandomAdvertisementsData();
+
+const map = createMap(activateForm);
+
+const marker = addMainMarker(map);
+marker.on('moveend', markerMoveEndHandler);
+
+addAdvertisementsMarkers(randomData, map);
+
